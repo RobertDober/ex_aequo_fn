4fa9a535bdf85146e7dcee62ec5fd6cfbf2532c6
RobertDober <robert.dober@gmail.com>
2024-07-08 19:15:27 +0200

    initial

---
+++ lib/ex_aequo_fn.ex
@@ -0,0 +1,18 @@
+defmodule ExAequoFn do
+  @moduledoc """
+  Documentation for `ExAequoFn`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExAequoFn.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ex_aequo_fn_test.exs
@@ -0,0 +1,8 @@
+defmodule ExAequoFnTest do
+  use ExUnit.Case
+  doctest ExAequoFn
+
+  test "greets the world" do
+    assert ExAequoFn.hello() == :world
+  end
+end
